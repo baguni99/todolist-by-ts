758f42268bdd13d8cfb3b06c73ee10a821f42567
eunhee <eunhee9902@gmail.com>
2023-08-01 01:32:56 +0900

    제목 가운데 정렬, todo 나란히 정렬

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { styled } from "styled-components";
 
 interface Todo {
   //Todo가 가져야 할 속성들을 정의해서 객체로 만들기
@@ -42,72 +43,97 @@ const App = () => {
 
   return (
     <div>
-      <header>todoList by TypeScript</header>
-
-      <div>
-        <input
-          className="inputTitle"
-          type="text"
-          placeholder="제목"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          onKeyPress={(e) => {
-            if (e.key === "Enter" || e.currentTarget.nodeName === "BUTTON") {
-              handleAddTodo(todos.length + 1, title, content, false);
+      <Header>TodoList by TypeScript</Header>
+      <InputSpace>
+        <div>
+          <input
+            className="inputTitle"
+            type="text"
+            placeholder="제목"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            onKeyPress={(e) => {
+              if (e.key === "Enter" || e.currentTarget.nodeName === "BUTTON") {
+                handleAddTodo(todos.length + 1, title, content, false);
+              }
+            }}
+          />
+          <br />
+          <input
+            className="inputTodo"
+            type="text"
+            placeholder="내용"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            onKeyPress={(e) => {
+              if (e.key === "Enter" || e.currentTarget.nodeName === "BUTTON") {
+                handleAddTodo(todos.length + 1, title, content, false);
+              }
+            }}
+          />
+        </div>
+        <div>
+          <button
+            onClick={() =>
+              handleAddTodo(todos.length + 1, title, content, false)
             }
-          }}
-        />
-        <input
-          className="inputTodo"
-          type="text"
-          placeholder="내용"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          onKeyPress={(e) => {
-            if (e.key === "Enter" || e.currentTarget.nodeName === "BUTTON") {
-              handleAddTodo(todos.length + 1, title, content, false);
-            }
-          }}
-        />
-      </div>
-      <div>
-        <button
-          onClick={() => handleAddTodo(todos.length + 1, title, content, false)}
-        >
-          추가
-        </button>
-      </div>
+          >
+            +
+          </button>
+        </div>
+      </InputSpace>
       <h2>Working...</h2>
-      {todos
-        .filter((todo) => !todo.isDone)
-        .map((todo) => (
-          <div key={todo.id}>
-            <>
-              <h3>{todo.id}</h3>
-              <h4>{todo.title}</h4>
-              <div>{todo.content}</div>
+      <WorkingSpace>
+        {todos
+          .filter((todo) => !todo.isDone)
+          .map((todo) => (
+            <div key={todo.id}>
+              <>
+                <h3>{todo.id}</h3>
+                <h4>{todo.title}</h4>
+                <div>{todo.content}</div>
 
-              <button onClick={() => handleRemoveTodo(todo.id)}>삭제</button>
-              <button onClick={() => handleDoneTodo(todo.id)}>완료</button>
-            </>
-          </div>
-        ))}
+                <button onClick={() => handleRemoveTodo(todo.id)}>삭제</button>
+                <button onClick={() => handleDoneTodo(todo.id)}>완료</button>
+              </>
+            </div>
+          ))}
+      </WorkingSpace>
       <h2>Done</h2>
-      {todos
-        .filter((todo) => todo.isDone)
-        .map((todo) => (
-          <div key={todo.id}>
-            <>
-              <h3>{todo.id}</h3>
-              <h4>{todo.title}</h4>
-              <div>{todo.content}</div>
+      <DoneSpace>
+        {todos
+          .filter((todo) => todo.isDone)
+          .map((todo) => (
+            <div key={todo.id}>
+              <>
+                <h3>{todo.id}</h3>
+                <h4>{todo.title}</h4>
+                <div>{todo.content}</div>
 
-              <button onClick={() => handleRemoveTodo(todo.id)}>삭제</button>
-              <button onClick={() => handleCancelTodo(todo.id)}>취소</button>
-            </>
-          </div>
-        ))}
+                <button onClick={() => handleRemoveTodo(todo.id)}>삭제</button>
+                <button onClick={() => handleCancelTodo(todo.id)}>취소</button>
+              </>
+            </div>
+          ))}
+      </DoneSpace>
     </div>
   );
 };
+
+const Header = styled.header`
+  font-size: 30px;
+  line-height: 42px;
+  text-align: center;
+`;
+const InputSpace = styled.div`
+  text-align: center;
+`;
+const WorkingSpace = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+const DoneSpace = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
 export default App;
